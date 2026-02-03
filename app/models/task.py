7180455382a5fdb2929452d047c74d55b3c3f6e2
NikosKl <nikos.klavi@gmail.com>
2026-02-03 12:36:53 +0200

    refactor(task): add timestamps, inder user_id, changed tablename to tasks

--- app/models/task.py
+++ app/models/task.py
@@ -1,14 +1,15 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Integer, ForeignKey
-
+from sqlalchemy import String, Integer, ForeignKey, DateTime
+from datetime import datetime, UTC
 from app.db.base import Base
 
 class Task(Base):
-    __tablename__ = "task"
+    __tablename__ = "tasks"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(255), nullable=False)
     description: Mapped[str] = mapped_column(String(255), nullable=True)
     priority: Mapped[int] = mapped_column(Integer, nullable=False, default=1)
     status: Mapped[str] = mapped_column(String(255), nullable=False, default="pending")
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(UTC))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True, nullable=False)
