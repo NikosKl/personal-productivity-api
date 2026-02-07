6fdf177ba0b9acb807816841d4d9a0f12889814f
NikosKl <nikos.klavi@gmail.com>
2026-02-07 16:28:30 +0200

    feat(tasks): add update and delete endpoints

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from starlette import status
 from sqlalchemy.orm import Session
 from app.core.security import get_current_user
 from app.db.session import get_db
 from app.models.user import User
-from app.schemas.task import TaskRead, TaskCreate
-from app.services.tasks import create_task, get_user_tasks
+from app.schemas.task import TaskRead, TaskCreate, TaskUpdate
+from app.services.tasks import create_task, get_user_tasks, TaskNotFoundError, update_task, delete_task
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
@@ -17,3 +18,19 @@ def create_tasks(task_data: TaskCreate, db: Session = Depends(get_db), current_u
 def read_tasks(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     user_tasks = get_user_tasks(db, current_user)
     return user_tasks
+
+@router.put("/{task_id}", response_model=TaskRead)
+def update_task_route(task_id: int, task_data: TaskUpdate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    try:
+        task = update_task(db, current_user, task_id, task_data)
+        return task
+    except TaskNotFoundError:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+@router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task_route(task_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    try:
+        delete_task(db, current_user, task_id)
+    except TaskNotFoundError:
+        raise HTTPException(status_code=404, detail="Task not found")
+
