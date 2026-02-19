e075e86a75330176f0c0bf908e38dd279566f918
NikosKl <nikos.klavi@gmail.com>
2026-02-19 17:28:21 +0200

    test: switch tests database from SQLite to PostgreSQL

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,3 +1,5 @@
+from dotenv import load_dotenv
+load_dotenv()
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -6,14 +8,13 @@ from starlette.testclient import TestClient
 import os
 
 os.environ['JWT_SECRET'] = 'test_secret'
+TEST_DATABASE_URL = os.environ.get('TEST_DATABASE_URL')
 
 from app.db.base import Base
 from app.db.session import get_db
 from app.main import app
 
-TEST_DATABASE_URL = "sqlite:///./test.db"
-
-engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(TEST_DATABASE_URL)
 
 TestSession = sessionmaker(bind=engine)
 
@@ -28,9 +29,9 @@ app.dependency_overrides[get_db] = override_get_db
 
 @pytest.fixture(autouse=True)
 def setup_test_db():
+    Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
     yield
-    Base.metadata.drop_all(bind=engine)
 
 @pytest.fixture()
 def client():
