4122029cac123cf07acb582e94b63e3834337139
NikosKl <nikos.klavi@gmail.com>
2026-02-25 15:23:35 +0200

    feat: add log_habit service

--- app/services/habits.py
+++ app/services/habits.py
@@ -1,8 +1,13 @@
+from datetime import date, timedelta
+
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+
+from app.models import habit_logs
 from app.models.habit import Habits
+from app.models.habit_logs import HabitLogs
 from app.models.user import User
-from app.schemas.habit import HabitCreate, HabitUpdate
+from app.schemas.habit import HabitCreate, HabitUpdate, HabitLogCreate
 
 
 class HabitNotFoundError(Exception):
@@ -58,5 +63,45 @@ def delete_habit(db: Session, user: User, habit_id: int) -> None:
     db.delete(habit)
     db.commit()
 
+def log_habit(db: Session, user: User, habit_id: int, log_data: HabitLogCreate) -> HabitLogs:
+    stmt = select(Habits).where(Habits.id == habit_id, Habits.user_id == user.id)
 
+    habit = db.scalar(stmt)
 
+    if habit is None:
+        raise HabitNotFoundError('Habit not found')
+
+    log_date = log_data.log_date or date.today()
+
+    if habit.frequency == 'daily':
+        stmt = select(HabitLogs).where(
+            HabitLogs.habit_id == habit.id,
+            HabitLogs.user_id == user.id,
+            HabitLogs.log_date == log_date)
+    else:
+        start_of_week = log_date - timedelta(days=log_date.weekday())
+        end_of_week = start_of_week + timedelta(days=6)
+
+        stmt = select(HabitLogs).where(
+            HabitLogs.habit_id == habit.id,
+            HabitLogs.user_id == user.id,
+            HabitLogs.log_date >= start_of_week,
+            HabitLogs.log_date <= end_of_week
+        )
+
+    habit_log = db.scalar(stmt)
+
+    if habit_log is not None:
+        raise HabitAlreadyLoggedError('Habit already logged in')
+
+    new_habit_log = HabitLogs(
+        habit_id=habit.id,
+        user_id=user.id,
+        log_date=log_date,
+    )
+
+    db.add(new_habit_log)
+    db.commit()
+    db.refresh(new_habit_log)
+
+    return new_habit_log
