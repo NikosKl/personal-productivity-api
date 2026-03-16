960d0f99766be962efcdadeabebd52efc71897db
NikosKl <nikos.klavi@gmail.com>
2026-03-16 16:12:00 +0200

    chore: remove debug print

--- migrations/env.py
+++ migrations/env.py
@@ -4,10 +4,7 @@ from app.db.base import Base
 from app.models import user, task, habit, habit_logs
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
-
-print("db: ",DATABASE_URL)
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
